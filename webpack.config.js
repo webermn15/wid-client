ab4fcb8ff8ffd9c916e5323aa631a73247997818
Michael Weber <webermn15@gmail.com>
2018-10-17 16:57:27 -0500

    installed and set up webpack configuration for development, including the dev-server, basic react component, and babel for loading jsx, js, and css

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+	entry: [
+		'./app/index.js'
+	],
+	output: {
+		filename: 'index_bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [
+      { 
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+        	loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+        	loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {loader: 'style-loader'}, 
+          {loader: 'css-loader'}
+        ]
+      }
+		]
+	},
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist')
+  },
+	mode: 'development'
+}
